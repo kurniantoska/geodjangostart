55d9605bab9b3341ffa6acfa68f7acdbd8f386df
Riska K A <alternative.xen@gmail.com>
2018-12-25 00:43:41 +0000

    all here

---
+++ shops/views.py
@@ -0,0 +1,19 @@
+from django.views import generic
+from django.contrib.gis.geos import fromstr
+from django.contrib.gis.db.models.functions import Distance
+from django.contrib.gis.geos import Point
+from shops.models import Shop
+
+
+longitude = 123.0589686
+latitude = 0.5363535
+
+user_location = Point(longitude, latitude, srid=4326)
+
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = 'shops'
+    queryset = Shop.objects.annotate(distance=Distance('location',
+    user_location)
+    ).order_by('distance')[0:6]
+    template_name = 'index_shops.html'
